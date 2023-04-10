b83ece6c69045e6faeeaa3f3cdfc8228d6909295
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-04-10 17:17:05 -0400

    fix mobile last slide

--- static/lib/main.js
+++ static/lib/main.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
 					}, {
 						breakpoint: 768, // sm/xs
 						settings: {
-							slidesToShow: 2,
+							slidesToShow: 1,
 							slidesToScroll: 1,
 							infinite: false,
 						},
